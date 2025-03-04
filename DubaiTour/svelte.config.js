4c9f10a42b2c7337445b0240b8a45719938f0554
ASpaceboy <Chimangakenge1@gmail.com>
2025-03-04 17:57:21 +0000

    hyhuiy

--- DubaiTour/svelte.config.js
+++ DubaiTour/svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
 		alias: {
-			'$css': path.resolve('src/lib/css'),
+			$css: 'src/lib/css',
 			$components: 'src/lib/components',
 		}
 	}
