76e5dca31e6547870c2f68f378e6ddc41b1017bc
ASpaceboy <Chimangakenge1@gmail.com>
2025-03-06 19:47:34 +0000

    it

--- src/lib/components/AttractionCard.svelte
+++ src/lib/components/AttractionCard.svelte
@@ -9,10 +9,7 @@
           d="M20 9V5H4V9H20ZM20 11H4V19H20V11ZM3 3H21C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3ZM5 12H8V17H5V12ZM5 6H7V8H5V6ZM9 6H11V8H9V6Z"
         ></path>
       </svg>
-      <p class="para">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi laboriosam
-        at voluptas minus culpa deserunt delectus sapiente inventore pariatur
-      </p>
+      <p class="para"><slot name="subtitle"></slot></p>
     </div>
   </div>
   
@@ -34,7 +31,7 @@
       align-items: flex-start;
       gap: 24px;
       padding: 36px;
-      border-radius: 22px;
+      border-radius: 20px;
       color: #ffffff;
       overflow: hidden;
       background: #0a3cff;
@@ -80,7 +77,7 @@
     .content .para {
       z-index: 1;
       opacity: 1;
-      font-size: 18px;
+      font-size: 16px;
       transition: all 0.48s cubic-bezier(0.23, 1, 0.32, 1);
     }
   
